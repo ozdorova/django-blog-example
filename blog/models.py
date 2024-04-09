1441c5389acf6dbc3f859a38196af04112c01b23
--global <--global>
2024-04-09 20:21:33 +0300

    add templates

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+
 class Post(models.Model):
     
+    #статус публикации
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     
+    #поля
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -20,9 +28,15 @@ class Post(models.Model):
                             choices=Status.choices,
                             default=Status.DRAFT)
     author = models.ForeignKey(User,
-                                on_delete=models.CASCADE,
-                                related_name='blog_posts')
+                            on_delete=models.CASCADE,
+                            related_name='blog_posts')
     
+    #менеджеры
+    objects = models.Manager()
+    published = PublishedManager()
+    
+    
+    #метаданные
     class Meta:
         ordering = [
             '-publish'
