7f7157ff742e6f8450e669fc8e8e3ce739c52862
--global <--global>
2024-04-10 17:12:50 +0300

    add markdown filter(not work yet)

--- blog/templatetags/custom_tags.py
+++ blog/templatetags/custom_tags.py
@@ -1,12 +1,15 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 
 register = template.Library()
 
 @register.filter
 def ru_plural(value, variants):
+    # фильтр для склонения в ед и множ числе
     variants = variants.split(',')
     value = abs(int(value))
     
@@ -22,17 +25,25 @@ def ru_plural(value, variants):
 
 @register.simple_tag
 def total_posts():
+    # количество постов
     return Post.published.count()
 
 
 @register.inclusion_tag('blog/post/latest_post.html')
 def show_latest_posts(count=5):
+    # последние посты
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
 
 
 @register.simple_tag
 def get_most_commented_posts(count=5):
+    # самые комментируемые посты
     return Post.published.annotate(
         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
